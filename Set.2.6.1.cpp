20920118b123311bd51ddf3160239f7a5bd830e8
Eric Scott <scotteric2021@gmail.com>
2021-04-07 17:16:54 -0500

    Added Set.2.6.1

---
+++ Set.2.6.1.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+
+
+void printArray(const int array[], const int arrayLen)
+{
+    for(int i = 0; i < arrayLen; ++i)
+    {
+        cout << array[i];
+        if (i = arrayLen - 1)
+        {
+            cout << ", ";
+        }
+    }
+    
+}
+
+void reverseArray(array[], arrayLen)
+{
+    
+}

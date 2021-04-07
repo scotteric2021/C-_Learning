3b51a1776c2e0c4af6fb09fc92a52ca207359801
Eric Scott <scotteric2021@gmail.com>
2021-04-07 12:57:44 -0500

    Update to Set2.4.1_6.cpp to include 2.4.2

--- Set2.4.1_6.cpp
+++ Set2.4.1_6.cpp
@@ -8,7 +8,10 @@ function for taking the sum of two doubles.
 #include <iostream>
 
 using namespace std;
-/*
+
+int sum3(const int, const int, const int);
+int sum4(const int, const int, const int, const int);
+
 int sum(const int x, const int y)
 {
     return (x + y);
@@ -17,16 +20,36 @@ int sum(const int x, const int y)
 
 int main()
 {
-    int num1, num2;
-    cout << "Enter the first integer";
+    int num1, num2, num3, num4;
+    cout << "This program will sum the values of 2 to 4 integers.\n" << "Enter 0 to terminate sequence.\n";
+    cout << "Enter the first integer: ";
     cin >> num1;
-    cout << "Enter the second integer";
+    cout << "Enter the second integer: ";
     cin >> num2;
-    cout << "The sum is " << sum(num1, num2);
+    cout << "Enter the third integer: ";
+    cin >> num3;
+    if (num3 > 0)
+    {
+        cout << "Enter the fourth integer: ";
+        cin >> num4;
+        if (num4 > 0)
+        {
+            cout << "The sum is " << sum4(num1, num2, num3, num4);
+        }
+        else
+        {
+            cout << "The sum is " << sum3(num1, num2, num3);
+        }
+    }
+    else
+    {
+        cout << "The sum is " << sum(num1, num2);
+    }
+    
     return 0;
 }
-*/
 
+/*
 double sum(const double x, const double y)
 {
     return (x + y);
@@ -41,4 +64,32 @@ int main()
     cin >> num2;
     cout << "The sum is " << sum(num1, num2);
     return 0;
+}
+*/
+/*4.2
+Explain why, given your functions from part 1, sum(1, 10.0) is a syntax error. (Hint: Think
+about promotion and demotion – the conversion of arguments between types in a function
+call. Remember that the compiler converts between numerical types for you if necessary.) [1
+point] 
+*/
+
+/* sum(1, 10.0) would be a syntax error in the above functions because the first number given is an integer,
+while the second is a floating point number.  The compiler would promote the integer to a float to match the second
+unless it was defined as a double.  My program has a way around this by defining both inputs as doubles.
+If the integer version was used then the output would only show the truncated integer version of the addition.
+*/
+
+/* 4.3
+Write 2 more functions such that you can find the sum of anywhere between 2 and 4 integers
+by writing sum(num1, num2, ...).
+*/
+
+int sum3 (const int x, const int y, const int z)
+{
+    return (x + y + z);
+}
+
+int sum4 (const int x, const int y, const int z, const int a)
+{
+    return (x + y + z + a);
 }
